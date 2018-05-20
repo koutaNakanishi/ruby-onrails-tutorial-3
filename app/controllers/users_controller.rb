e8a1f35291ae51399630d3455c0a72abe59ae8cb
vagrant <nizi26i26@gmail.com>
2018-05-20 09:34:50 +0100

    Finish chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
   def new
+		@user=User.new
+	#	debugger
   end
+	
+	def create
+	#	@user=User.new(params[:user])丸々paramsのhashを渡すのは良くない
+		@user=User.new(user_params)
+		if@user.save
+			flash[:success]="Welcome to the Sample App! 登録完了"
+			#初めは登録完了ページを出したい
+			redirect_to @user
+		else 
+			render 'new'
+		end
+	end
 
 	def show
 		@user=User.find(params[:id])
 	end
+
+	private#ネットワーク経由で実行できない（攻撃される危険性が少ない？）ヘルパー
+		def user_params
+			params.require(:user).permit(:name,:email,:password,:password_confirmation)
+		end
 
 end
